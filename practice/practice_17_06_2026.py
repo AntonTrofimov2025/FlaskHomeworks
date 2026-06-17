2298a7cd74d02c5a9369f47a63fb5c3ac870804e
AntonTrofimov2025 <xerurganton@gmail.com>
2026-06-17 16:58:03 +0200

    little fixes

--- practice/practice_17_06_2026.py
+++ practice/practice_17_06_2026.py
@@ -1,6 +1,13 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
+
+# users_json = os.path.dirname(os.path.abspath(__file__))
+# # abspath - полный путь к скрипту (__file__),
+# # затем отрезаем имя скрипта и оставляем только путь (dirname)
 
 @app.route('/')
 def hello_world():
@@ -21,6 +28,18 @@ def float_square(number):
 @app.route('/<path:your_path>')
 def reversed_path(your_path):
     return f'Your reversed path: {your_path[::-1]}'
+
+# @app.route('/users')
+# def users():
+#     file_path = os.path.join(users_json, 'users.json')
+#     with open(file_path, 'r', encoding='utf8') as f:
+#         return jsonify(json.load(f))
+
+@app.route('/users')
+def users():
+    print(f"JSON Path: {os.path.abspath('./practice/users.json')}")
+    with open(os.path.abspath('./practice/users.json'), 'r', encoding='utf-8') as f:
+        return jsonify(json.load(f))
 
 if __name__ == "__main__":
     app.run(debug=True)
